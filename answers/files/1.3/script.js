b350b4c77a5e23bb0e9f47288ef54852777ac2be
igor <igor-oleynik@gmail.com>
2021-11-26 15:43:12 +0200

    descriptive commit message

---
+++ answers/files/1.3/script.js
@@ -0,0 +1,7 @@
+let admin;
+let name = 'Jhon';
+admin = name;
+
+
+
+
---
+++ answers/files/1.4/script.js
@@ -0,0 +1,13 @@
+// наша планета
+let ourEarth;
+
+// текущий пользователь
+let currentUser = 'Igor';
+
+// пользователь верифицирован
+let userVerified = true;
+
+// максимальная цена
+const maxPrice = '199';
+let price = maxPrice;
+alert(price); // 199
