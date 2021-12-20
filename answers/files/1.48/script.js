c64e30b91d3a7411de4a92e8e3f2cfe425fb4fa0
igor-oleynik <igor-oleynik@gmail.com>
2021-12-20 10:41:55 +0200

    add

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,26 +1,52 @@
+let operator;
+let expression;
+let leftOperand;
+let rightOperand;
+let computation;
+let result;
+
 function onLeftOperandChange(value) {
-  if (onLeftOperandChange === 'string') {
-    return;
-  }
   console.log('onLeftOperandChange', value)
+  leftOperand = value;
 }
 
 function onRightOperandChange(value) {
-  if (onRightOperandChange === 'string') {
-    return;
-  }
   console.log('onRightOperandChange', value)
+  rightOperand = value;
 }
 
 function onOperatorChange(value) {
   console.log('onOperatorChange', value)
 }
 
+const ADDITION = 'ADDITION'; // +
+const SUBTRACTION = 'SUBTRACTION'; // -
+const MULTIPLICATION = 'MULTIPLICATION'; // *
+const EXPONENTIATION = 'EXPONENTIATION'; // xª
+const DIVISION = 'DIVISION'; // /
+const MODULUS = 'MODULUS'; // %
+const ROOT = 'ROOT'; // √
+
 function onSubmit () {
-  return setResult;
-  console.log('onSubmit')
+  let writeLeftOperand = ;
+  switch (operator) {
+    case 'ADDITION':
+      computation  = leftOperand + rightOperand;
+      expression =
+
+    case 'SUBTRACTION':
+    case 'MULTIPLICATION':
+    case 'EXPONENTIATION':
+    case 'DIVISION':
+    case 'MODULUS':
+    case 'ROOT' :
+
+      console.log('onSubmit')
+  }
+  setResult('')
 }
 
 function onClear () {
   console.log('onClear')
+  expression = null;
 }
