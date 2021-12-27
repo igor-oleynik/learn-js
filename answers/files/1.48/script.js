9219ab581444fd468b1a8c78d227414d544e380f
igor-oleynik <igor-oleynik@gmail.com>
2021-12-27 08:55:16 +0200

    add

--- answers/files/1.48/script.js
+++ answers/files/1.48/script.js
@@ -1,24 +1,3 @@
-let operator;
-let expression;
-let leftOperand;
-let rightOperand;
-let computation;
-let result;
-
-function onLeftOperandChange(value) {
-  console.log('onLeftOperandChange', value)
-  leftOperand = value;
-}
-
-function onRightOperandChange(value) {
-  console.log('onRightOperandChange', value)
-  rightOperand = value;
-}
-
-function onOperatorChange(value) {
-  console.log('onOperatorChange', value)
-}
-
 const ADDITION = 'ADDITION'; // +
 const SUBTRACTION = 'SUBTRACTION'; // -
 const MULTIPLICATION = 'MULTIPLICATION'; // *
@@ -27,26 +6,74 @@ const DIVISION = 'DIVISION'; // /
 const MODULUS = 'MODULUS'; // %
 const ROOT = 'ROOT'; // √
 
+let operator = ADDITION;
+let expression;
+let leftOperand;
+let rightOperand;
+let computation;
+let result;
+let resultLeftOperand;
+
+function onLeftOperandChange(value) {
+  // console.log('onLeftOperandChange', value)
+  leftOperand = Number(value);
+};
+
+function onRightOperandChange(value) {
+  // console.log('onRightOperandChange', value)
+  rightOperand = Number(value);
+};
+
+function onOperatorChange(value) {
+  // console.log('onOperatorChange', value)
+  operator = value;
+};
+
 function onSubmit () {
-  let writeLeftOperand = ;
+  let resultLeftOperand = expression ?? leftOperand;
   switch (operator) {
     case 'ADDITION':
       computation  = leftOperand + rightOperand;
-      expression =
+      expression = `${resultLeftOperand}  + ${rightOperand}`;
+      break;
 
     case 'SUBTRACTION':
+      computation  = leftOperand - rightOperand;
+      expression = `${resultLeftOperand} - ${rightOperand}`;
+      break;
+
     case 'MULTIPLICATION':
+      computation  = leftOperand * rightOperand;
+      expression = `${resultLeftOperand} * ${rightOperand}`;
+      break;
+
     case 'EXPONENTIATION':
+      computation  = leftOperand ** rightOperand;
+      expression = `${resultLeftOperand} xª ${rightOperand}`;
+      break;
+
     case 'DIVISION':
+      computation  = leftOperand / rightOperand;
+      expression = `${resultLeftOperand} / ${rightOperand}`;
+      break;
+
     case 'MODULUS':
+      computation  = leftOperand % rightOperand;
+      expression = `${resultLeftOperand} % ${rightOperand}`;
+      break;
+
     case 'ROOT' :
+      computation  = leftOperand ** rightOperand;
+      expression = `${resultLeftOperand} √ ${rightOperand}`;
+      break;
 
-      console.log('onSubmit')
+      // console.log('onSubmit')
   }
-
-}
+  setResult(computation);
+  setExpression(`${expression} = ${computation}`);
+};
 
 function onClear () {
-  console.log('onClear')
+  // console.log('onClear')
   expression = null;
-}
+};
